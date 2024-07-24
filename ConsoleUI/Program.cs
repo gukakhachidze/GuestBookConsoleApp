f87c12d05e851df28563fc0f8c1d5d106797973b
Guka Khachidze <guka.khachidze@gmail.com>
2024-07-24 12:19:26 +0400

    Separate Logic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using GuestBookLibrary.Models;
+﻿using GuestBookLibrary.GuestBookLogic;
+using GuestBookLibrary.Models;
 
 // Capture the information about each guest (assumtion is at least one guest and unknow maximum)
 // info to capture: First name, Last name, message to the host
@@ -7,43 +8,10 @@
 
 List<GuestModel> guests = new List<GuestModel>();
 
-GetGuestInformation(guests);
-PrintGuestInformation(guests);
+GuestBook.GetGuestInformation(guests);
+GuestBook.PrintGuestInformation(guests);
 
 
-static void PrintGuestInformation(List<GuestModel> guests)
-{
-    foreach (GuestModel guest in guests)
-    {
-        Console.WriteLine(guest.GuestInfo);
-    }
-}
-static void GetGuestInformation(List<GuestModel> guests)
-{
-    string moreGuestsComing = "";
 
-    do
-    {
-        GuestModel guest = new GuestModel();
-        
-        guest.FirstName = GetInfoFromConsole("What is your first name: ");
-        guest.LastName = GetInfoFromConsole("What is your last name: ");
-        guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
-        moreGuestsComing = GetInfoFromConsole("Are more guests coming (yes/no): ");
 
-        guests.Add(guest);
 
-        Console.Clear();
-
-    } while (moreGuestsComing.ToLower() == "yes");
-}
-
-static string GetInfoFromConsole(string message)
-{
-    string output = "";
-
-    Console.Write(message);
-    output = Console.ReadLine();
-
-    return output;
-}
---
+++ GuestBookLibrary/GuestBookLogic/GuestBook.cs
@@ -0,0 +1,45 @@
+﻿using GuestBookLibrary.Models;
+
+namespace GuestBookLibrary.GuestBookLogic
+{
+    public static class GuestBook
+    {
+        public static void PrintGuestInformation(List<GuestModel> guests)
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+
+        public static void GetGuestInformation(List<GuestModel> guests)
+        {
+            string moreGuestsComing = "";
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+
+                guest.FirstName = GetInfoFromConsole("What is your first name: ");
+                guest.LastName = GetInfoFromConsole("What is your last name: ");
+                guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
+                moreGuestsComing = GetInfoFromConsole("Are more guests coming (yes/no): ");
+
+                guests.Add(guest);
+
+                Console.Clear();
+
+            } while (moreGuestsComing.ToLower() == "yes");
+        }
+
+        private static string GetInfoFromConsole(string message)
+        {
+            string output = "";
+
+            Console.Write(message);
+            output = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
